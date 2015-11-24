14c57ac04356c26d5596cf03739d7ee653494193
alex <shawcs.t@gmail.com>
2015-11-24 10:48:58 +0100

    view first

---
+++ src/main/java/org/esilv/ibo/movies/view/home.java
@@ -0,0 +1,39 @@
+package org.esilv.ibo.movies.view;/**
+ * Created by alex on 24/11/2015.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class home extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello World!");
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 850, 500));
+        primaryStage.show();
+    }
+}
